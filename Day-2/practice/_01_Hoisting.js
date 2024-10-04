ba4676f1029d0b8d3164a658bdc2fa8cb4bd5bdc
priya varshney <priyavarshney081003@gmail.com>
2024-10-04 11:17:59 +0530

    Hoisting done

--- Day-2/practice/_01_Hoisting.js
+++ Day-2/practice/_01_Hoisting.js
@@ -37,11 +37,50 @@ function addone(num) {
     return num+1;
 }
 
-addTwo(5);
-const addTwo = function(num){
-    return num+2;
-}
+// addTwo(5);
+// const addTwo = function(num){
+//     return num+2;
+// }
 
 // why this add two cant be accessed
 // there are difference in the two definitions
+
+
+/*
+console.log(a);
+function myFunc() {
+    console.log("inside");
+}
+myFunc();
+var a = 10;
+// here hoisting in case of var occurs
+*/
+
+console.log(a);
+function myFunc() {
+    console.log("inside");
+}
+myFunc();
+let a = 10;
+
+// ReferenceError: Cannot access 'a' before initialization
+// in case of let and const hoisting is still occuring but in temporal dead zone
+// TDZ is the region between the point of declaration and the point of initialization
+
+
+// TDZ is the the block where a variable is inaccessible until the moment the computer initializes it with a value.
+
+// in let and const js knows that the variable is created but cant access its value before initialization
+
+var b=10;
+function fun() {
+    console.log(b);
+}
+
+fun();
+
+// on calling fun function it is creatign new MEmory creation phase and code execution context it will find b if not present it will find in its lexical environment 
+// it went to ite parent's MCP i.e. Lexical environment
+
+// scope - local memory + lexical environment of its parent
 
